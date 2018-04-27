2873e7ffe7cdf3425c1547370a2a6cb10c0fa76d
Tim Blake <timblake17@hotmail.com>
2018-04-26 22:49:56 -0400

    3problems done

--- template/js/index.js
+++ template/js/index.js
@@ -1,15 +1,78 @@
-// A left rotation operation on an array of size N shifts each of the array's elements N units to the left. For example, if 2 left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2].
-// Given an array of N integers and a number, D, perform D left rotations on the array. Then print the updated array as a single line of space-separated integers.
+// function DNAStrand(dna){
+// var arr=[];
+//   /// loop stough string
+//   for(var i=0; i<dna.length; i++){
+//   var letter=dna[i];
+//   if(letter == 'A'){
+//    letter='T'
+//    arr.push(letter);
+//    console.log(arr)
+//   } else if(letter == 'T'){
+//    letter='A';
+//    arr.push(letter);
+//
+//   }else if(letter == 'C'){
+//    letter='G';
+//    arr.push(letter);
+//
+//   }else if(letter == 'G'){
+//    letter='C';
+//    arr.push(letter);
+//
+//
+//   }
+// }
+// return arr.join();
+//
+// }
 
-function shift(input,rot){
-  var arr=[];
-  for (var i=0; i<rot; i++){
-    var num=input[i];
-    arr.push(input.shift(num));
-
-  }
-  var combined=input.concat(arr);
-  return (combined);
+///You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+function getMiddle(s)
+{
+var len=s.length
+  if(len%2==0){
+  var letter=s.slice(len/2-1,len/2+1);
+}else{
+  letter=s.slice(len/2,len/2+1)
+}
+return letter
 }
 
-console.log(shift([3, 4, 5, 1, 2],2));
+console.log(getMiddle('test'))
+
+//
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+
+function filter_list(l) {
+  var final=[];
+  for(var i=0; i<l.length; i++){
+  var index=l[i];
+  var type= typeof index;
+
+  if(type=='number'){
+    final.push(index)
+      }
+    }
+     return final
+  }
+
+
+  function multiplicationTable(width, height){
+    var table = [];
+    var row = [];
+
+    for (i = 0; i < width; i++){
+      for (j = 0; j < height; j++){
+        row.push((i + 1)*(j + 1));
+      }
+      table.push(row);
+      row = [];
+    }
+    return table;
+  }
+
+    //first num is how many arrays
+    ///second num is how many numbers in the array
+    //for loop for second number
+  }
